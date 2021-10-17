409ecb41bb3a27d7973caf9a52af673cbeabd654
cassiocappellari <cassiocappellari@gmail.com>
2021-10-17 20:04:22 -0300

    feat: add exception handler at create transaction controller

--- server/src/controllers/CreateTransactionController.ts
+++ server/src/controllers/CreateTransactionController.ts
@@ -4,14 +4,18 @@ import CreateTransactionService from "../services/CreateTransactionService";
 
 class CreateTransactionController {
 
-  async importTransactions(request: Request, response: Response) {
-    const { file } = request;
-    const { buffer } = file as any;
+  async importTransactions(request: Request, response: Response) { 
+    try {
+      const { file } = request;
+      const { buffer } = file as any;
 
-    const transactionsList = await fileReader(buffer);
-    await CreateTransactionService.importTransactions(transactionsList);
-
-    return response.status(201).json({ message: 'transactions successfully created' });
+      const transactionsList = await fileReader(buffer);
+      await CreateTransactionService.importTransactions(transactionsList);
+  
+      return response.status(201).send({ message: "transactions successfully created" });
+    } catch(error) {
+      return response.status(500).send({ message: "internal server error" })
+    }
   };
 
 }
